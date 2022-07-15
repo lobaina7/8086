cbc4604a9781194527d4ff40cad24d64bd827886
Lobaina7 <alobainad@gmail.com>
2022-07-15 13:55:12 -0400

    Primer teclado funcionando

--- main.asm
+++ main.asm
@@ -9,9 +9,7 @@ org 400h
 mov ax,0042h
 mov ds,ax
 
-jmp start
 
-start:
 ; Configuracion del 8255 PP1
      mov dx, DIR_PP1
      mov al, WC1
@@ -21,62 +19,98 @@ start:
     mov dx, DIR_PP2
     mov al, WC2
     out dx, al
+
+    mov dx, PB1
+    mov al, 0
+    out dx, al
+; inicializar variables
+start:    CALL TECLADO1
+          JMP start
+
+ TECLADO1 PROC NEAR
+          MOV CX,5
+	  MOV BL,0FEH ;calcula la máscara de fila
+ TECLA:   MOV DX, PB1
+          MOV AL,BL
+          OUT DX,AL
+          MOV DX,PC1
+          IN  AL,DX
+          TEST AL,1
+          JZ TEC_DECODIFICADOR1
+          TEST AL,2
+          JZ TEC_DECODIFICADOR2
+CONTINUE: ROL BL,1
+	  DEC CX
+	  JCXZ FIN
+	  JMP TECLA
+
+
+ TEC_DECODIFICADOR1:
+    CALL DECODIFICADOR1_TECLADO1
+    JMP CONTINUE
+ TEC_DECODIFICADOR2:
+    CALL DECODIFICADOR2_TECLADO1
+    JMP CONTINUE
+
+ FIN:
+     RET
+ TECLADO1 ENDP
+
+ DECODIFICADOR1_TECLADO1 PROC NEAR USES BX AX
+
+   XOR BH,BH
+   NOT BL
+   MOV DX,PA1
+   MOV AL,BL
+   OUT DX,AL
+   MOV DX,PC1
+   MOV AL,BH
+   OUT DX,AL
+
+ RET
+ DECODIFICADOR1_TECLADO1 ENDP
+
+
+ DECODIFICADOR2_TECLADO1 PROC NEAR USES BX AX
+
+   OR BH,0FFh
+   ROL BX,1
+   ROL BX,1
+   ROL BX,1
+   ROL BX,1
+   ROL BX,1
+   ROL BH,1
+   ROL BH,1
+   ROL BH,1
+   ROL BH,1
+   NOT BX
+   MOV DX,PA1
+   MOV AL,BL
+   OUT DX,AL
+   MOV DX,PC1
+   MOV AL,BH
+   OUT DX,AL
+
+ RET
+ DECODIFICADOR2_TECLADO1 ENDP
 end1:
 	jmp   end1
 
-;***************************************************
-;		Función de salida para control de périféricos
-;***************************************************
 
-OUTPUT PROC NEAR
-
-       PUSH AL
-       PUSH DX
-
-           mov dx, addr
-           mov al, data
-           out dx, al
-
-       POP DX
-  	   POP AL
-  	   RET
-OUTPUT ENDP
-
-;***************************************************
-;		Función de entrada para control de périféricos
-;***************************************************
-
-INPUT PROC NEAR
-
-       PUSH AL
-       PUSH DX
-
-          mov dx, addr
-          in al, dx
-          mov result, al
-
-       POP DX
-  	   POP AL
-  	   RET
-INPUT ENDP
 
 .data ;Data segment - Segmento de datos
 ;***************************************************
 ;		Configuración del 8255A
 ;***************************************************
-DIR_PP1  EQU F006H
-WC1       EQU 83H
-PCA       EQU F004H
-PBA       EQU F002H
-PAA       EQU F000H
-DIR_PP2  EQU F086H
-WC2       EQU 8AH
-PC2       EQU F084H
-PB2       EQU F082H
-PA2       EQU F080H
-
-temp   db 720 dup(37)
-addr   dw ?
-data   db ?
-result db ?
+DIR_PP1   EQU 0FE06H
+WC1       EQU 81H
+PC1       EQU 0FE04H
+PB1       EQU 0FE02H
+PA1       EQU 0FE00H
+DIR_PP2   EQU 0FF06H
+WC2       EQU 89H
+PC2       EQU 0FF04H
+PB2       EQU 0FF02H
+PA2       EQU 0FF00H
+LEDS      dW   1,2,4,6,8,16,32,64,128,256,400h,800h
 END
